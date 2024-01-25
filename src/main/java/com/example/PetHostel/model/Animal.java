141a2d0ab67bab563e936f098f388b88881dd92a
balamat <balazs.mate@flowacademy.hu>
2024-01-25 13:58:27 +0100

    First commit

---
+++ src/main/java/com/example/PetHostel/model/Animal.java
@@ -0,0 +1,22 @@
+package com.example.PetHostel.model;
+
+public abstract class Animal {
+
+    private Long id;
+    private PetOwner owner;
+    private String petName;
+    private AnimalType animalType;
+    private Integer age;
+    private Gender gender;
+    private Boolean isNeutered;
+    private PetCharacter petCharacter;
+    private String specialNeeds;
+
+    //picture URL from a web API
+    private String pictureURL;
+
+
+
+
+
+}
---
+++ src/main/java/com/example/PetHostel/model/Reservation.java
@@ -0,0 +1,14 @@
+package com.example.PetHostel.model;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Reservation {
+
+    private LocalDate startingDate;
+    private LocalDate finishingDate;
+    private List<PetService> orderedPetServices;
+
+
+
+}
