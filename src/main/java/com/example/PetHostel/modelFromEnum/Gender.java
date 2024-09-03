7855236ff5389992d6e9016450ca7192efcade66
matebolic <balmate@gmail.com>
2024-09-03 16:35:10 +0200

    Add bootstrap dateTimePicker libraries, creating new templates

--- src/main/java/com/example/PetHostel/modelFromEnum/Gender.java
+++ src/main/java/com/example/PetHostel/modelFromEnum/Gender.java
@@ -1,6 +1,12 @@
 package com.example.PetHostel.modelFromEnum;
 
 public enum Gender {
-    MALE,
-    FEMALE
+    MALE("male"),
+    FEMALE("female");
+
+    private String name;
+
+    Gender(String name) {
+        this.name = name;
+    }
 }
